7061b0c19116732a397664768e690417f6d1fad2
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-09-07 10:19:10 -0700

    favicon and pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+var now = moment();
+console.log(now);
+
 //▓▓▓▓▓ arrays for table ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 var tableArray = [
@@ -22,17 +25,22 @@ var tableArray = [
         firstTime: "02:30",
         frequency: 30,
     },
-]
-    
+];
 
-
-
+//THIS INFORMATION IS USED TO FIND
+    //*TRAIN NAME*
+    //*DESTINATION*
+    //*FREQUENCY*
+    //*NEXT ARRIVAL*
+    //*MINUTES AWAY*
 
 //▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 
 function renderTable() {
+    for (var i = 0; i < tableArray.length; i++) {
 
+    }
 }
 
 
@@ -108,4 +116,28 @@ function renderTable() {
 
 
 
-//▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//▓▓▓▓▓ pseudocode ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+/* 
+
+click event for SUBMIT button
+ - takes .val of each form group and runs the CALCULATE function and appends all the neccesary info to the table.
+
+CALCULATE function
+ - uses moment.js and logic to take the .val of #first-train-time and #train-freq and output *next arrival and *minutes away, along with name, destination and frequency, directly.
+    //if (now < first-train-time)
+        find minutes between now and first-train-time --> MINUTESAWAY
+    //else if (now)
+    //this output is pushed as an object to the table array, then the RENDERTABLE function is called.
+
+RENDERTABLE function
+ - takes the information in the tableArray and appends that information in the form of a table.
+
+
+
+*/
+
+
+
+
+
